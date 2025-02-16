fd511aca84cbb1a249a548430477820dbba0b9eb
isadauzaker <jvictordauzaker@gmail.com>
2025-02-16 12:44:42 -0300

    StoryCubesGame3.0

--- Story Cubes Game/src/components/Body.jsx
+++ Story Cubes Game/src/components/Body.jsx
@@ -1,18 +1,10 @@
 import './Body.css';
-import { Icon } from '@iconify/react/dist/iconify.js';
+import SorteioAleatorio from './SorteioAleatorio';
 
 function Body() {
   return (
     <div className='container-body'>
-
-      <div className='container-livros'>
-        <Icon icon="game-icons:archive-register" className='livro' />
-        <div className='container-icones'></div>
-        <Icon icon="game-icons:archive-research" className='livro' />
-      </div>
-
-      <button className='sortear'>Sortear imagens</button>
-
+      <SorteioAleatorio />
     </div>
   );
 }
--- Story Cubes Game/src/components/SorteioAleatorio.jsx
+++ Story Cubes Game/src/components/SorteioAleatorio.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import './SorteioAleatorio.css';
+import { Icon } from '@iconify/react/dist/iconify.js'
+import { useState } from 'react';
 
 function SorteioAleatorio() {
 
-    const []
+  const [imagens, setImagens] = useState([]);
+
+  const todasImagens = [
+    "game-icons:dragon-head",
+    "game-icons:light-bulb",
+    "game-icons:treasure-map",
+    "game-icons:hourglass",
+    "game-icons:rocket",
+    "game-icons:scroll-unfurled",
+    "game-icons:castle",
+    "game-icons:magic-lamp",
+    "game-icons:pirate-flag",
+  ];
+
+  const sortearImagens = () => {
+    const novasImagens = [];
+    for (let i = 0; i < 3; i++) { // Sorteia 3 imagens
+      const indiceAleatorio = Math.floor(Math.random() * todasImagens.length);
+      novasImagens.push(todasImagens[indiceAleatorio]);
+    }
+    setImagens(novasImagens);
+  };
 
   return (
-    <div>
+    <div className='container-sorteioAleatorio'>
       
+      <div className='container-livros'>
+        <Icon icon="game-icons:archive-register" className='livro' />
+
+        <div className='container-icones'>
+          {imagens.map((img, index) => (
+            <Icon key={index} icon={img} className='icone' />
+          ))}
+        </div>
+
+        <Icon icon="game-icons:archive-research" className='livro' />
+      </div>
+
+      <button className='sortear' onClick={sortearImagens}>Sortear imagens</button>
+
     </div>
   )
 }
