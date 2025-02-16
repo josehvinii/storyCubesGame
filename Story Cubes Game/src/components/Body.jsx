f6b3f2dd8c0a8bd3fa36ca138a596bc9aa2d6e9f
isadauzaker <jvictordauzaker@gmail.com>
2025-02-15 23:21:58 -0300

    StoryCubesGame2.0

--- Story Cubes Game/src/components/Body.jsx
+++ Story Cubes Game/src/components/Body.jsx
@@ -1,17 +1,20 @@
-import './Body.css'
+import './Body.css';
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 function Body() {
   return (
     <div className='container-body'>
 
-        <div className='container-icones'>
+      <div className='container-livros'>
+        <Icon icon="game-icons:archive-register" className='livro' />
+        <div className='container-icones'></div>
+        <Icon icon="game-icons:archive-research" className='livro' />
+      </div>
 
-        </div>
+      <button className='sortear'>Sortear imagens</button>
 
-        <button className='sortear'>Sortear imagens</button>
-      
     </div>
-  )
+  );
 }
 
-export default Body
+export default Body;
--- Story Cubes Game/src/components/Navbar.jsx
+++ Story Cubes Game/src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import './Navbar.css'
+import { Icon } from '@iconify/react/dist/iconify.js'
 
 function Navbar() {
   return (
     <div className='container-navbar'>
+
+    <Icon icon="game-icons:dice-six-faces-one" className='dado' />
+    <Icon icon="game-icons:dice-six-faces-two" className='dado' />
+    <Icon icon="game-icons:dice-six-faces-three" className='dado' />
         
-        <h1 className='titulo'>Seja Bem vindo ao Story Cubes!</h1>
+    <h1 className='titulo'>Seja Bem vindo ao Story Cubes!</h1>
+
+    <Icon icon="game-icons:dice-six-faces-four" className='dado' />
+    <Icon icon="game-icons:dice-six-faces-five" className='dado' />
+    <Icon icon="game-icons:dice-six-faces-six" className='dado' />
 
     </div>
   )
