a2e63158a50a5a51a2e0032135d39f13eb9b91b3
josehvinii <jvictordauzaker@gmail.com>
2025-02-17 17:25:30 -0300

    continuação...

--- Story Cubes Game/src/components/SorteioAleatorio.jsx
+++ Story Cubes Game/src/components/SorteioAleatorio.jsx
@@ -7,109 +7,45 @@ function SorteioAleatorio() {
   const [imagens, setImagens] = useState([]);
 
   const todasImagens = [
-    "game-icons:dragon-head",
-    "game-icons:light-bulb",
-    "game-icons:treasure-map",
-    "game-icons:hourglass",
-    "game-icons:rocket",
-    "game-icons:scroll-unfurled",
-    "game-icons:castle",
-    "game-icons:magic-lamp",
-    "game-icons:pirate-flag",
-    "game-icons:chess-knight",
-    "game-icons:feather",
-    "game-icons:fire",
-    "game-icons:skull",
-    "game-icons:anchor",
-    "game-icons:moai",
-    "game-icons:robot",
-    "game-icons:spy",
-    "game-icons:coffee-cup",
-    "game-icons:crown",
-    "game-icons:ghost",
-    "game-icons:heart",
-    "game-icons:key",
-    "game-icons:medal",
-    "game-icons:moon",
-    "game-icons:orbital",
-    "game-icons:plant",
-    "game-icons:puzzle-piece",
-    "game-icons:shuriken",
-    "game-icons:shield",
-    "game-icons:skull-crossbones",
-    "game-icons:snowflake",
-    "game-icons:sun",
-    "game-icons:sword",
-    "game-icons:tower",
-    "game-icons:ufo",
-    "game-icons:umbrella",
-    "game-icons:viking-helmet",
-    "game-icons:wrench",
-    "game-icons:yin-yang",
-    "game-icons:crystal-ball",
-    "game-icons:griffin-symbol",
-    "game-icons:dragonfly",
-    "game-icons:hourglass",
-    "game-icons:scroll",
-    "game-icons:crab",
-    "game-icons:musical-notes",
-    "game-icons:compass",
-    "game-icons:mask",
-    "game-icons:anubis",
-    "game-icons:zeppelin",
-    "game-icons:candle",
-    "game-icons:quill",
-    "game-icons:thunderbolt",
-    "game-icons:wolf-head",
-    "game-icons:phoenix",
-    "game-icons:water-drop",
-    "game-icons:ancient-sword",
-    "game-icons:tentacle",
-    "game-icons:griffin",
-    "game-icons:magic-swirl",
-    "game-icons:anvil",
-    "game-icons:laser-blast",
-    "game-icons:eyeball",
-    "game-icons:robot-antennas",
-    "game-icons:witch-hat",
-    "game-icons:evil-moon",
-    "game-icons:fireball",
-    "game-icons:exploding-planet",
-    "game-icons:chimney",
-    "game-icons:crystal-shine",
-    "game-icons:bat",
-    "game-icons:skull-ring",
-
+    "game-icons:3d-stairs", "game-icons:alarm-clock", "game-icons:airplane-departure", "game-icons:banknote", "game-icons:bed",
+"game-icons:baby-face", "game-icons:bathtub", "game-icons:beach-ball", "game-icons:beech", "game-icons:binoculars",
+"game-icons:blunderbuss", "game-icons:bow-tie", "game-icons:brain", "game-icons:brick-wall", "game-icons:briefcase",
+"game-icons:bus", "game-icons:burn", "game-icons:capybara", "game-icons:candle-light", "game-icons:cheese-wedge",
+"game-icons:cat", "game-icons:chicken", "game-icons:drop", "game-icons:flip-flops", "game-icons:hatchet",
+"game-icons:labrador-head", "game-icons:kite", "game-icons:large-paint-brush", "game-icons:microphone", "game-icons:mushroom-cloud",
+"game-icons:ninja-head", "game-icons:paint-brush", "game-icons:overdose", "game-icons:pig", "game-icons:pirate-captain",
+"game-icons:poison-bottle", "game-icons:pizza-slice", "game-icons:plastic-duck", "game-icons:polo-shirt", "game-icons:popcorn",
+"game-icons:power-lightning", "game-icons:punch-blast", "game-icons:retro-controller", "game-icons:smartphone", "game-icons:steam-locomotive",
+"game-icons:sunglasses", "game-icons:unlit-bomb", "game-icons:vampire-cape", "game-icons:ufo", "game-icons:umbrella",
+"game-icons:witch-face", "game-icons:werewolf", "game-icons:wanted-reward", "game-icons:village", "game-icons:vintage-robot",
+"game-icons:tv", "game-icons:talk", "game-icons:spy", "game-icons:strongbox", "game-icons:spiked-dragon-head",
+"game-icons:snowing", "game-icons:sofa", "game-icons:slingshot", "game-icons:shopping-cart", "game-icons:scissors",
+"game-icons:raining", "game-icons:road", "game-icons:pocket-radio", "game-icons:open-book", "game-icons:locked-chest"
   ];
 
   const sortearImagens = () => {
-    const novasImagens = [];
-    for (let i = 0; i < 9; i++) { // Sorteia as imagens
-      const indiceAleatorio = Math.floor(Math.random() * todasImagens.length);
-      novasImagens.push(todasImagens[indiceAleatorio]);
-    }
-    setImagens(novasImagens);
+    const novaImagem = todasImagens[Math.floor(Math.random() * todasImagens.length)];
+    setImagens([novaImagem]); // Atualiza a imagem sorteada
   };
 
   return (
     <div className='container-sorteioAleatorio'>
-      
       <div className='container-livros'>
         <Icon icon="game-icons:archive-register" className='livro' />
-
+        
         <div className='container-icones'>
           {imagens.map((img, index) => (
-            <Icon key={index} icon={img} className='iconesSorteados' /> // Ícones que estão no array e serão sorteados
+ 
+            <Icon key={img} icon={img} className='icone-sorteado' />
           ))}
         </div>
 
         <Icon icon="game-icons:archive-research" className='livro' />
       </div>
 
-      <button className='sortear' onClick={sortearImagens}>Sortear imagens</button>
-
+      <button className='sortear' onClick={sortearImagens}>Sortear imagem</button>
     </div>
-  )
+  );
 }
 
-export default SorteioAleatorio
+export default SorteioAleatorio;
